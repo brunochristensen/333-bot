69b7c1039086202f97ec1847839093180eae099b
brunochristensen <brunojchris@gmail.com>
2023-11-27 12:46:25 -0600

    updated adherence to OOP

---
+++ src/accountabilitycog.py
@@ -0,0 +1,34 @@
+import datetime
+import discord
+from discord.ext import commands, tasks
+
+class AccountabilityCog(commands.Cog):
+
+  def __init__(self, bot):
+    self.bot = bot
+    self.my_task.start()
+
+  def cog_unload(self):
+    self.my_task.cancel()
+
+  @tasks.loop(time=datetime.time(hour=0, minute=0))  #TODO: Implement DST
+  async def my_task(self):
+    view = AccountabilityDropdownView()
+    await self.bot.send('Pick your favourite colour:', view=view)
+    
+class AccountabilityDropdown(discord.ui.Select):
+  def __init__(self):
+      options = [
+          discord.SelectOption(label='Sec+', description='Up early to go pass the exam'),
+          discord.SelectOption(label='Sick Call', description='Remember to let an MTL know.')
+      ]
+      super().__init__(placeholder='Choose your reason for absence:', min_values=1, max_values=1, options=options)
+
+  async def callback(self, interaction: discord.Interaction):
+      await interaction.response.send_message(f'Reason for absence: {self.values[0]}')
+
+class AccountabilityDropdownView(discord.ui.View):
+  def __init__(self):
+      super().__init__()
+      self.add_item(AccountabilityDropdown())
+  
--- src/main.py
+++ src/main.py
@@ -1,54 +1,39 @@
 # https://discordpy.readthedocs.io/en/stable/
 
-import os
-
 import discord
 from discord.ext import commands
-import coghandler
+from accountabilitycog import AccountabilityCog
 from dotenv import load_dotenv
+import logging
+import os
 
-###################################
-# Global vars and objects
-###################################
 load_dotenv()
 
-intents = discord.Intents.default()
-intents.message_content = True
+class Bot(commands.Bot):
+  def __init__(self):
+    intents = discord.Intents.default()
+    intents.message_content = True
+    super().__init__(command_prefix=commands.when_mentioned_or('!'), intents=intents)
+    
+  async def on_ready(self):
+    print(f'Logged in as {self.user}' )
+    await self.add_cog(AccountabilityCog(self))
 
-client = commands.Bot(command_prefix='!', intents=intents)
+bot = Bot()
 
-
-@client.event
-async def on_ready():
-  print('Logged in as {0.user}'.format(client))
-  await client.add_cog(coghandler.AccountabilityCog(client))
-
-
-@client.event
+@bot.event
 async def on_message(message):
   #on_message triggers on the bot's own messages
-  if message.author == client.user:
+  if message.author == bot.user:
     return
   #Stops on_message from preventing @client.command() being executed
-  await client.process_commands(message)
+  await bot.process_commands(message)
 
-
-###################################
-# Commands
-###################################
-@client.command()
+@bot.command()
 async def hello(ctx):
   await ctx.send('Hello!')
 
+token = os.getenv("DISCORD_TOKEN") or ""
+log_handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+bot.run(token, log_handler=log_handler, log_level=logging.DEBUG)
 
-###################################
-# Connection Service
-###################################
-try:
-  token = os.getenv("DISCORD_TOKEN") or ""
-  client.run(token)
-except discord.HTTPException as e:
-  if e.status == 429:
-    print("Discord servers denied the connection for making too many requests")
-  else:
-    raise e
