4f188973f33c7ee76bb8c23593711d05a04f13d7
brunochristensen <brunojchris@gmail.com>
2023-11-27 15:34:32 -0600

    accountabilitycog init fixed

--- src/accountabilitycog.py
+++ src/accountabilitycog.py
@@ -14,21 +14,21 @@ class AccountabilityCog(commands.Cog):
   @tasks.loop(time=datetime.time(hour=0, minute=0))  #TODO: Implement DST
   async def my_task(self):
     view = AccountabilityDropdownView()
-    await self.bot.send('Pick your favourite colour:', view=view)
+    await self.bot.send('Pick your reason for absence:', view=view)
     
 class AccountabilityDropdown(discord.ui.Select):
   def __init__(self):
-      options = [
-          discord.SelectOption(label='Sec+', description='Up early to go pass the exam'),
-          discord.SelectOption(label='Sick Call', description='Remember to let an MTL know.')
-      ]
-      super().__init__(placeholder='Choose your reason for absence:', min_values=1, max_values=1, options=options)
+    options = [
+      discord.SelectOption(label='Sec+', description='Up early to go pass the exam'),
+      discord.SelectOption(label='Sick Call', description='Remember to let an MTL know.')
+    ]
+    super().__init__(placeholder='Reason for absence:', min_values=1, max_values=1, options=options)
 
   async def callback(self, interaction: discord.Interaction):
-      await interaction.response.send_message(f'Reason for absence: {self.values[0]}')
+    await interaction.response.send_message(f'Reason for absence: {self.values[0]}')
 
 class AccountabilityDropdownView(discord.ui.View):
   def __init__(self):
-      super().__init__()
-      self.add_item(AccountabilityDropdown())
+    super().__init__()
+    self.add_item(AccountabilityDropdown())
   
