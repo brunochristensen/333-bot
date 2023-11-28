30230076751c32d2dfaa31d906d98b0b0c7abc4f
brunochristensen <brunojchris@gmail.com>
2023-11-28 13:26:47 -0600

    added dropdown and data struct

--- src/accountabilitycog.py
+++ src/accountabilitycog.py
@@ -1,34 +1,68 @@
-import datetime
 import discord
 from discord.ext import commands, tasks
+from dotenv import load_dotenv
+import os
+
 
 class AccountabilityCog(commands.Cog):
+    def __init__(self, bot):
+        load_dotenv()
+        self.bot = bot
+        self.channel = self.bot.get_channel(int(os.getenv("ACCOUNTABILITY_CHANNEL_ID")))
+        self.struct = DataStruct(self.bot)
+        self.my_task.start()
 
-  def __init__(self, bot):
-    self.bot = bot
-    self.my_task.start()
+    # time=datetime.time(hour=0, minute=0))  #TODO: Implement DST
+    @tasks.loop(minutes=1)
+    async def my_task(self):
+        await self.channel.send("Menus", view=SelectView())
 
-  def cog_unload(self):
-    self.my_task.cancel()
-
-  @tasks.loop(time=datetime.time(hour=0, minute=0))  #TODO: Implement DST
-  async def my_task(self):
-    view = AccountabilityDropdownView()
-    await self.bot.send('Pick your reason for absence:', view=view)
+    @my_task.before_loop
+    async def before_sendmessage(self):
+        await self.bot.wait_until_ready()
+        #Delete old message
+        #Purge data structure
+        #reload data structure
+        
+        
+class DataStruct():
+    def __init__(self, bot):
+        self.dict = dict()
+        self.guild = bot.get_guild(int(os.getenv("GUILD_ID")))
+        self.role = self.guild.get_role(int(os.getenv("STUDENT_ROLE_ID")))
+        for user in self.role.members:
+            self.dict[user.nick] = ""
     
-class AccountabilityDropdown(discord.ui.Select):
-  def __init__(self):
-    options = [
-      discord.SelectOption(label='Sec+', description='Up early to go pass the exam'),
-      discord.SelectOption(label='Sick Call', description='Remember to let an MTL know.')
-    ]
-    super().__init__(placeholder='Reason for absence:', min_values=1, max_values=1, options=options)
+    def add_data(self, nickname, text):
+        self.dict.update({nickname: text})
 
-  async def callback(self, interaction: discord.Interaction):
-    await interaction.response.send_message(f'Reason for absence: {self.values[0]}')
+class Select(discord.ui.Select):
+    def __init__(self):
+        options = [
+            discord.SelectOption(label="Option 1", description="This is option 1!"),
+            discord.SelectOption(label="Option 2", description="This is option 2!"),
+            discord.SelectOption(label="Option 3", description="This is option 3!"),
+        ]
+        super().__init__(
+            placeholder="Select an option", max_values=1, min_values=1, options=options
+        )
 
-class AccountabilityDropdownView(discord.ui.View):
-  def __init__(self):
-    super().__init__()
-    self.add_item(AccountabilityDropdown())
-  
+    async def callback(self, interaction: discord.Interaction):
+        if self.values[0] == "Option 1":
+            interaction.user.display_name
+            await interaction.response.send_message(
+                content="This is the first option from the entire list!", ephemeral=True
+            )
+
+        elif self.values[0] == "Option 2":
+            await interaction.response.send_message(
+                "This is the second option from the list entire wooo!", ephemeral=True
+            )
+        elif self.values[0] == "Option 3":
+            await interaction.response.send_message("Third One!", ephemeral=True)
+
+
+class SelectView(discord.ui.View):
+    def __init__(self, *, timeout=180):
+        super().__init__(timeout=timeout)
+        self.add_item(Select())
--- src/main.py
+++ src/main.py
@@ -9,31 +9,38 @@ import os
 
 load_dotenv()
 
+
 class Bot(commands.Bot):
-  def __init__(self):
-    intents = discord.Intents.default()
-    intents.message_content = True
-    super().__init__(command_prefix=commands.when_mentioned_or('!'), intents=intents)
-    
-  async def on_ready(self):
-    print(f'Logged in as {self.user}' )
-    await self.add_cog(AccountabilityCog(self))
+    def __init__(self):
+        intents = discord.Intents.default()
+        intents.message_content = True
+        intents.members = True
+        super().__init__(
+            command_prefix=commands.when_mentioned_or("!"), intents=intents
+        )
+
+    async def on_ready(self):
+        print(f"Logged in as {self.user}")
+        await self.add_cog(AccountabilityCog(self))
+
 
 bot = Bot()
 
+
 @bot.event
 async def on_message(message):
-  #on_message triggers on the bot's own messages
-  if message.author == bot.user:
-    return
-  #Stops on_message from preventing @client.command() being executed
-  await bot.process_commands(message)
+    # on_message triggers on the bot's own messages
+    if message.author == bot.user:
+        return
+    # Stops on_message from preventing @client.command() being executed
+    await bot.process_commands(message)
+
 
 @bot.command()
 async def hello(ctx):
-  await ctx.send('Hello!')
+    await ctx.send("Hello!")
+
 
 token = os.getenv("DISCORD_TOKEN") or ""
-log_handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+log_handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
 bot.run(token, log_handler=log_handler, log_level=logging.DEBUG)
-
